e187e259c4423a82c8112d7a6c06df5539909318
floki61 <flo61ki@gmail.com>
2023-06-25 14:12:23 +0100

    push

---
+++ js.js
@@ -0,0 +1,14 @@
+let customersArray = ['Custy Stomer', 'C. Oostomar', 'C.U.S. Tomer', 3432434, 'Custo Mer', 'Custopher Ustomer', 3432435, 'Kasti Yastimeur'];
+
+//Write Your Code here:
+function CheckcustomArray():void{
+  let i = 0;
+  while(custmersArray[i])
+  {
+    if(typeof customersArray[i] != 'string'){
+      console.log(`Type error: ${el} should be a string!`);
+    }
+    i++;
+  }
+}
+
