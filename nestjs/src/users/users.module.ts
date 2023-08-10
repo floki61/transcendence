68fa96fcb6b84860c0a4cd900a3f714324ae8c1c
floki61 <flo61ki@gmail.com>
2023-08-10 13:38:40 +0100

    Welcome

--- nestjs/src/users/users.module.ts
+++ nestjs/src/users/users.module.ts
@@ -3,12 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     JwtModule.register({}),
   ],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
   controllers: [UsersController]
 })
 export class UsersModule {}
