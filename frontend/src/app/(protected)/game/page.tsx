d43f405c1f34f7d9993a499de1a65a65e7e75966
saad-ennour <saadennourfarhan@gmail.com>
2023-11-11 20:43:24 +0100

    bot and live game separated

--- frontend/src/app/(protected)/game/page.tsx
+++ frontend/src/app/(protected)/game/page.tsx
@@ -3,13 +3,14 @@ import Game from '@/components/Game';
 import Botgame from '@/components/Botgame';
 import {GameProvider} from "@/context/gameSocket"
 
-export default function App() {
+export default function page() {
+
 
    return (
       <GameProvider>
-      <div className="flex justify-center">
-         <Game />
-      </div>
+         <div className="flex justify-center">
+            <Game />
+         </div>
       </GameProvider>
    );
 }
